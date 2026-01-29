1d23c1a9c61791c4f65753f4a1800b42bbb9c718
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2026-01-29 07:59:36 +0500

    chore: Add icons to assets and mock data to project

---
+++ SwiftUI-Apple-Frameworks/Screens/NewScreen/Models/FrameworkDTO.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworkDTO.swift
+//  SwiftUI-Apple-Frameworks
+//
+//  Created by Bakhrom Usmanov on 29/01/26.
+//
+
+import Foundation
+
+struct FrameworkDTO {
+    let name: String
+    let imageName: String
+    let urlString: String
+    let description: String
+}
+
+
